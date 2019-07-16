5cedc4878561d912ba923a0072b0ebebf5a91db2
Caleb Oki <caleboki@gmail.com>
2019-07-16 16:36:44 +0100

    initial commit

---
+++ server/database/migrations/2019_06_30_231736_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('question', 250);
+            $table->string('imageName', 50);
+            $table->string('option1', 50);
+            $table->string('option2', 50);
+            $table->string('option3', 50);
+            $table->string('option4', 50);
+            $table->integer('answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
